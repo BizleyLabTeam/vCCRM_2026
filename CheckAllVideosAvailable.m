a5a6d31cd68013e45d835322f52f858809f78280
AV laptop <j.bizley@ucl.ac.uk>
2026-02-11 11:03:28 +0000

    mac compatibility

--- CheckAllVideosAvailable.m
+++ CheckAllVideosAvailable.m
@@ -4,22 +4,22 @@ vList = [];
 for ii = 1:length(wavList)
     if Video == 2
         if strfind(wavList{ii},'sentences')
-            vList{ii} = fullfile('video\Freeze', [wavList{ii}(11:end-4) '.mp4']);
+            vList{ii} = fullfile('video','Freeze', [wavList{ii}(11:end-4) '.mp4']);
         else
-            vList{ii} = fullfile('video\Freeze', [wavList{ii}(1:end-4) '.mp4']);
+            vList{ii} = fullfile('video','Freeze', [wavList{ii}(1:end-4) '.mp4']);
         end
     
     elseif Video == 3
          if strfind(wavList{ii},'sentences')
-            vList{ii} = fullfile('video\Disk', [wavList{ii}(11:end-4) '.mp4']);
+            vList{ii} = fullfile('video','Disk', [wavList{ii}(11:end-4) '.mp4']);
         else
-            vList{ii} = fullfile('video\Disk', [wavList{ii}(1:end-4) '.mp4']);
+            vList{ii} = fullfile('video','Disk', [wavList{ii}(1:end-4) '.mp4']);
          end
     else
         if strfind(wavList{ii},'sentences')
-            vList{ii} = fullfile('video\Full', [wavList{ii}(11:end-4) '.mp4']);
+            vList{ii} = fullfile('video','Full', [wavList{ii}(11:end-4) '.mp4']);
         else
-            vList{ii} = fullfile('video\Full', [wavList{ii}(1:end-4) '.mp4']);
+            vList{ii} = fullfile('video','Full', [wavList{ii}(1:end-4) '.mp4']);
         end
     end
 end
